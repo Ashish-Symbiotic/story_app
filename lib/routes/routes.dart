effd27a68044815029d4840f88c874729d0c7790
Ashish <Ashish_symbiotic@outlook.com>
2022-02-19 04:09:17 +0530

    "New Interface added"

---
+++ lib/routes/routes.dart
@@ -0,0 +1,9 @@
+class MyRoutes{
+  static  String AttandanceRoute ="/attan";
+  static  String MarkAttanRoute ="/myattan";
+  static String MainPageRoute ="/mainP";
+  static String PaymentMainRoute ="/mainPay";
+
+
+
+}
